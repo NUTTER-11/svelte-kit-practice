f8b7f3020e5519faa2b7bdfa2b751da1160521f5
NUTTER-11 <parulp8800@gmail.com>
2024-09-23 09:46:57 +0530

    third -commit

--- helloworld/src/routes/api/comments/[commentId]/+server.js
+++ helloworld/src/routes/api/comments/[commentId]/+server.js
@@ -1,10 +1,29 @@
 import { comments } from "$lib/comments.js";
-import {json } from '@sveltejs/kit'
+import { json } from '@sveltejs/kit'
+import { request } from "http";
 
-export function GET(requestEvent){
-    const {params}=requestEvent;
-    const {commentId}=params;
-    const comment =comments.find((comment)=> comment.id ===parseInt(commentId))
+export function GET(requestEvent) {
+    const { params } = requestEvent;
+    const { commentId } = params;
+    const comment = comments.find((comment) => comment.id === parseInt(commentId))
     return json(comment)
 
 }
+export async function PATCH(requestEvent) {
+    const { request, params } = requestEvent
+    const { commentId } = params
+    const { text } = await request.json();
+    const comment = comments.find((comment) => comment.id === parseInt(commentId))
+    comment.text = text;
+    return json(comment);
+}
+export async function DELETE(requestEvent) {
+    const { params } = requestEvent
+    const { commentId } = params
+    const deletedcomment = comments.find((comment) => comment.id === parseInt(commentId))
+    const index =comments.findIndex((comment) => comment.id === parseInt(commentId))
+    comments.splice(index,1)
+    return json(deletedcomment)
+
+}
+
